091d3d2b6a782c2a1b4e35c2c892214d0818f5d2
13680905763 <177748749@qq.com>
2023-02-17 15:54:56 +0800

    feat update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,10 @@ import { defaultTheme, defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '悲伤猪的博客',
+  base: '/',
   description: 'i am sadPig',
   theme: defaultTheme({
+    logo: 'http://caibaojian.com/vuepress/hero.png',
     navbar: [
       {
         text: '首页',
@@ -11,15 +13,15 @@ export default defineUserConfig({
       },
       {
         text: '前端知识',
-        link: '/web'
+        link: '/web/'
       },
       {
         text: '计算机基础',
-        link: '/computer'
+        link: '/computer/'
       },
       {
         text: '关于生活',
-        link: '/life'
+        link: '/life/'
       },
       {
         text: 'Github',
@@ -29,6 +31,61 @@ export default defineUserConfig({
         text: '掘金',
         link: 'https://juejin.cn/user/215917184954583'
       }
-    ]
+    ],
+    sidebar: {
+      '/web/': [
+        {
+          text: 'HTML&CSS',
+          children: [
+            {
+              text: 'HTML',
+              link: '/web/html-css/html/'
+            },
+            {
+              text: 'CSS',
+              link: '/web/html-css/css/'
+            }
+          ]
+        },
+        {
+          text: 'JavaScript',
+          children: [
+            {
+              text: 'js基础',
+              link: '/web/javascript/js基础/'
+            },
+            {
+              text: 'js高级'
+            }
+          ]
+        }
+      ],
+      '/computer/': [
+        {
+          text: '概念',
+          link: '/computer/concept.md'
+        }
+      ],
+      '/life/': [
+        {
+          text: '一些总结',
+          children: [
+            {
+              text: '2023的计划',
+              link: '/life/summarize/2023.md'
+            }
+          ]
+        },
+        {
+          text: '其他',
+          children: [
+            {
+              text: '谷歌插件推荐',
+              link: '/life/other/chrome.md'
+            }
+          ]
+        }
+      ]
+    }
   })
 })
